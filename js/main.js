5f01eeb7143628ed77e5865bbe5f5085dc666a58
jparrker <102701917+jparrker@users.noreply.github.com>
2023-12-03 08:57:02 -0500

    refactored js into functions

--- js/main.js
+++ js/main.js
@@ -1,11 +1,33 @@
-const accordionContainer = document.querySelector(".accordion-container");
+/**
+ * Finds the correct height of the accordion content
+ * @param {HTMLElement} accordion The accordion
+ */
+function getContentHeight(accordion) {
+  const accordionInner = accordion.querySelector(".accordion__inner");
 
+  if (accordion.classList.contains("is-open")) return 0;
+  return accordionInner.getBoundingClientRect().height;
+}
+
+/**
+ * Updates accordion
+ * @param {HTMLElement} accordion The accordion
+ * @param {Number} height px value of height to update
+ */
+function updateAccordion(accordion, height) {
+  const accordionContent = accordion.querySelector(".accordion__content");
+
+  accordion.classList.toggle("is-open");
+  accordionContent.style.height = `${height}px`;
+}
+
+const accordionContainer = document.querySelector(".accordion-container");
 accordionContainer.addEventListener("click", (event) => {
   const accordionHeader = event.target.closest(".accordion__header");
-  if (accordionHeader) {
-    const accordion = accordionHeader.parentElement;
-    accordion.classList.toggle("is-open");
-  } else {
-    console.log("not from header");
-  }
+  if (!accordionHeader) return;
+
+  const accordion = accordionHeader.parentElement;
+  const height = getContentHeight(accordion);
+
+  updateAccordion(accordion, height);
 });
